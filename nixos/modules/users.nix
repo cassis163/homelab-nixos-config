f24adf90d9cc3370cbb8e03f4b0c16b7b7595b45
cassis163 <c.aangeenbrug@protonmail.com>
2025-11-11 22:18:31 +0100

    Add public key

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -9,6 +9,7 @@
       hashedPassword = "$y$j9T$8w6neZcY5Xw3K6iTIxQJr/$i7fv0qBQ9gW7VLiEEsiDUCVIfiJB/OBSYmj2vZ8GFE/";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFF3T+xZq949jvEQGszDu4s2QqYsOpYwq3Gcv6tpPKrt caspera@PERSONAL-LAPTOP-CA"
       ];
       extraGroups = [
         "wheel"
