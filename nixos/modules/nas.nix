3116cd6adb92fa07ebe26ae8e81ded15962fec27
cassis163 <c.aangeenbrug@protonmail.com>
2025-11-21 22:25:04 +0100

    Add explicit Mac export for /export/chess

--- nixos/modules/nas.nix
+++ nixos/modules/nas.nix
@@ -16,8 +16,10 @@
     extraNfsdConfig = '''';
     exports = ''
       /export         192.168.11.0/24(rw,fsid=0,no_subtree_check)
-      /export/media   192.168.11.11(rw,nohide,insecure,no_subtree_check)
-      /export/chess   192.168.11.0/24(rw,nohide,insecure,no_subtree_check)
+
+      /export/media   192.168.11.0/24(rw,nohide,insecure,no_subtree_check)
+
+      /export/chess   192.168.11.0/24(rw,nohide,insecure,no_subtree_check) 192.168.11.253(rw,nohide,insecure,no_subtree_check)
     '';
   };
 
