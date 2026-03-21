160458b8db447ea68996d8e3d2bfb939a18deeda
cassis163 <c.aangeenbrug@protonmail.com>
2026-03-21 17:57:33 +0100

    Update SSH keypair

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -5,7 +5,7 @@
       hashedPassword = "$y$j9T$8w6neZcY5Xw3K6iTIxQJr/$i7fv0qBQ9gW7VLiEEsiDUCVIfiJB/OBSYmj2vZ8GFE/";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFF3T+xZq949jvEQGszDu4s2QqYsOpYwq3Gcv6tpPKrt caspera@PERSONAL-LAPTOP-CA"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINwQQE8fUoPG8UPcF4zreGn1z+R0YOwjvGI2floeGDEs c.aangeenbrug@protonmail.com"
       ];
       extraGroups = [
         "wheel"
